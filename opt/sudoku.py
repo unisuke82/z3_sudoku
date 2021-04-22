0347a8039f6be16e68ffe4d41bdac6e995566570
uni82 <mayu1368@gmail.com>
2021-04-22 16:05:50 +0900

    solve 4*4 sudoku

--- opt/sudoku.py
+++ opt/sudoku.py
@@ -11,9 +11,6 @@ X = [ [[Bool("P_%s_%s_%s" % (i, j, k+1))
         for i in range(sudoku_size)
     ]
 
-print(X)
-print(type(X))
-
 def have_value():
     have = And([ Or([And(X[i][j][a], Not(X[i][j][b]), Not(X[i][j][c]), Not(X[i][j][d]))
                      for a,b,c,d in itertools.permutations(range(sudoku_size))])
@@ -21,7 +18,6 @@ def have_value():
                  for i in range(sudoku_size)
                  ])
     solver.add(have)
-    print(have)
 
 def column_unique():
     cells_c = And([Or([And(X[0][j][a], X[1][j][b], X[2][j][c], X[3][j][d])
@@ -29,7 +25,6 @@ def column_unique():
                     for j in range(sudoku_size)
                ])
     solver.add(cells_c)
-    print(cells_c)
 
 def row_unique():
     row_c = And([ Or([And(X[i][0][a], X[i][1][b], X[i][2][c], X[i][3][d])
@@ -37,7 +32,6 @@ def row_unique():
                     for i in range(sudoku_size)
                     ])
     solver.add(row_c)
-    print(row_c)
 
 def square_unique():
     square_c = And([ Or([And(X[i*2][j*2][a], X[i*2+1][j*2][b], X[i*2][j*2+1][c], X[i*2+1][j*2+1][d])
@@ -46,27 +40,32 @@ def square_unique():
                      for j in range(int(math.sqrt(sudoku_size)))
                      ])
     solver.add(square_c)
-    print('square_c')
-    print(square_c)
 
 have_value()
 column_unique()
 row_unique()
 square_unique()
 
-print(solver.check())
-print(solver.model())
+solver.add(
+    And(
+        X[0][0][1],
+        X[0][1][0],
+        X[0][2][2],
+        X[3][1][3],
+        X[3][2][0],
+        X[3][3][1],
+    )
+)
 
+solver.check()
+solver.model()
 
-r = [ [ [ solver.model().evaluate(X[i][j][k]) for k in range(sudoku_size) ]
-      for j in range(sudoku_size) ]
-      for i in range(sudoku_size)
-      ]
+if solver.check()==sat:
+    r = [ [ [ solver.model().evaluate(X[i][j][k]) for k in range(sudoku_size) ].index(True)+1
+            for j in range(sudoku_size) ]
+          for i in range(sudoku_size)
+          ]
+    print(r)
 
-print_matrix(r)
-
-r = [ [ [ solver.model().evaluate(X[i][j][k]) for k in range(sudoku_size) ].index(True)
-        for j in range(sudoku_size) ]
-      for i in range(sudoku_size)
-      ]
-print_matrix(r)
+else:
+    print(solver.check())
